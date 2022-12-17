ab251552b16b9fdab9208b2ba04eb51885856a1d
pol-cova <83374367+pol-cova@users.noreply.github.com>
2022-12-17 00:42:07 -0600

    v0.0.3
    I add the connection to mongo DB to save the information of my users, also I add the support page to recopile bug, in the future I add a simple chatbot, to make a more faster answer to users

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ This api was created to register users to 'close-beta',the main use is only regi
 This version is the first stable version (v0.0.1) this version may needed a db connection to save the names, and emails
 
 # version-details
-* Version -> v0.0.2
+* Version -> v0.0.3
 * Support -> pm.interactive.games@gmail.com
 * Questions -> For more details please send a mail to support! :)
--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 # Import flask, and the packages needed for run the app
-from flask import Flask,redirect,render_template,url_for,request
+from flask import Flask,redirect,render_template,url_for,request, jsonify
 # Import this library needed for handling a config files and get the needed resources
 from configparser import ConfigParser
+# Import library for connect db
+from pymongo import MongoClient 
 
 # Import libraries to generate random serial-key
 import random
 import string
+
+# Import libraries to api function
+import datetime
 
 # Import necesary configuration files 
 config = ConfigParser()
@@ -16,12 +21,17 @@ SECRET_KEY = config.get('app', 'secret_key')
 # Import necesary configuration files for db
 configDB = ConfigParser()
 configDB.read('./config/db.config.cfg')
-
+db_client = configDB.get('db-uri', 'client')
 
 # Basic configuration for flask aplication
 app = Flask(__name__)
 # Import from config the main app secret key
 app.secret_key = SECRET_KEY
+
+# Define DB connection
+client = MongoClient(db_client)
+db = client.get_database('new_users')
+db_users = db.get_collection('new_users')
 
 # Main route -> this route render the register form in html to register users
 @app.route('/')
@@ -38,11 +48,46 @@ def api_register():
         email = request.form['email']
         # This condicional propositon validate if form doesn't be empty, in the case the form be empty return an error (403)
         if name != '' and email !='':
-            source = string.ascii_letters + string.digits
-            # This function generate a random serial code for user (in the moment this key is saved in the DB)
-            user_serial_key = ''.join((random.choice(source) for i in range(8)))
-            return render_template('success.html', name = name, email = email, serial = user_serial_key)
+            found_email = db_users.find_one({"email" : email })
+            if found_email:
+                return render_template("010.html")
+            else:
+                source = string.ascii_letters + string.digits
+                # This function generate a random serial code for user (in the moment this key is saved in the DB)
+                user_serial_key = ''.join((random.choice(source) for i in range(8)))
+                # This function register user into a db
+                new_user = {'name': name, 'email': email, 'serial-code': user_serial_key}
+                db_users.insert_one(new_user)
+                return render_template('success.html', name = name, email = email, serial = user_serial_key)
     return render_template('403.html')
+
+# Support routes-> this route is for recieve request from users
+# Main route
+@app.route('/deepblur/web-service/support')
+def support():
+    return render_template('support.html')
+# Route to handling errors report
+@app.route('/deepblur/web-services/support/request/status', methods = ['GET', 'POST'])
+def request_support():
+    if request.method == 'POST':
+        name = request.form['name']
+        error = request.form['error']
+        if name != '' and error != '':
+            date_for_request = datetime.datetime.now()
+            support_email = 'pm.interactive.games@gmail.com'
+            support_advice = 'Hello, We stay tuned into your comments, and sorry for your error, if the error persist please contact us for email :) !'
+            support_status = 'We recived your request!'
+            support_request_status = "Working on solving your bug! "
+            data = {
+                "Support message": support_advice,
+                "Support email": support_email,
+                "Name": name,
+                "Error or bug": error,
+                "Date of report": date_for_request,
+                "Support report": support_status,
+                "Actual status of report": support_request_status
+            }
+            return jsonify(data)
 
 # This is the function to start the main app
 if __name__ == '__main__':
